8868edbea95f035402f90a32ee9d8648a697e067
Irfan <irfan.uppsala@gmail.com>
2016-05-16 14:01:36 +0200

    Added simple jQuery examples

---
+++ js/app.js
@@ -0,0 +1,73 @@
+$(function() {
+  var state = true;
+  $( "#toggle" ).click(function() {
+    if ( state ) {
+      $( "#elem" ).animate({
+        backgroundColor: "#aabb00",
+        color: "#fff",
+        width: 1100
+      }, 1000 );
+    } else {
+      $( "#elem" ).animate({
+        backgroundColor: "#ccc",
+        color: "#000",
+        width: 1150
+      }, 1000 );
+    }
+    state = !state;
+  });
+});
+
+/* fold effect start JQuery*/
+$( "#squareToggle" ).click(function() {
+		$( "#square" ).toggle( "fold" );
+});
+
+/* scale effect start JQuery*/
+$( "#squareScale" ).click(function() {
+		$( "#square2" ).toggle({ effect: "scale", direction: "horizontal" });
+});
+
+/* scale effect start JQuery*/
+$("#draggable").draggable();
+
+$(function() {
+  $( "#slider" ).slider();
+
+});
+
+/* Button effects */
+$(function() {
+// run the currently selected effect
+function runEffect() {
+// get effect type from
+var selectedEffect = $( "#effectTypes" ).val();
+
+// most effect types need no options passed by default
+var options = {};
+// some effects have required parameters
+if ( selectedEffect === "scale" ) {
+  options = { percent: 0 };
+} else if ( selectedEffect === "transfer" ) {
+  options = { to: "#button", className: "ui-effects-transfer" };
+} else if ( selectedEffect === "size" ) {
+  options = { to: { width: 200, height: 60 } };
+}
+
+// run the effect
+$( "#effect" ).effect( selectedEffect, options, 500, callback );
+};
+
+// callback function to bring a hidden box back
+function callback() {
+setTimeout(function() {
+  $( "#effect" ).removeAttr( "style" ).hide().fadeIn();
+}, 1000 );
+};
+
+// set effect from select menu value
+$( "#effectTypes" ).change(function() {
+runEffect();
+return false;
+});
+});
